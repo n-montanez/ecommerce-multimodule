e27d3374ff424e8dbe16233c97baebc5bae1e9e8
Nicolás Montañez Velasco <nicolas.montanez@globant.com>
2025-06-10 09:27:52 -0500

    fix: datasource reference

--- product-catalog/src/main/java/com/montanez/products_catalog/ProductsCatalog.java
+++ product-catalog/src/main/java/com/montanez/products_catalog/ProductsCatalog.java
@@ -3,7 +3,7 @@ package com.montanez.products_catalog;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("/")
 public class ProductsCatalog extends Application {
     
 }
